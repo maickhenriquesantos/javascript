c94dd197dc2a280d811bfeeb04cc254edf2b1e43
Maick Henrique <contatomaickh@gmail.com>
2021-05-05 22:27:07 -0300

    Trabalhando com variaveis e constantes

---
+++ variaveis-constantes.js
@@ -0,0 +1,38 @@
+//var = variavel global
+//let = variavel no escopo
+//const = constante(nao muda)
+
+var nome = "Maick Henrique Santos";
+nome = "Maick Henrique";
+console.log(nome);
+
+{
+    let idade = 28;
+    console.log(idade);
+}
+
+const nome1 = "Henrique";
+console.log(nome1);
+
+var numero = 28;
+var nome2 = "Juca";
+var nome3 = 'João';
+
+var outraVariavel = true;
+
+var x = 2;
+var y = 3;
+var z = 4;
+var w = 5;
+
+var soma = x + y + z + w;
+var sub = w - z - y - x;
+var div = z / 4;
+var mod = w % y;
+var mult = x * y +(w * z);
+
+console.log("A soma total é :" + soma);
+console.log("A subtração total é :" + sub);
+console.log("A div total é :" + div);
+console.log("O resto da divisao é :" + mod);
+console.log("O valor da multiplicação é :" + mult);
